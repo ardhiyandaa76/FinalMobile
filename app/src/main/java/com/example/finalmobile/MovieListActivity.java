dcf338a948fc972034f691d233f53593cf11f306
AgungArdhiyanda <ardhiyandaak21h@student.unhas.ac.id>
2023-06-06 19:07:47 +0800

    feat(MovieApi) :  add MovieApi interface for movie search
    - Adding interface to handle movie search request
    - implement searchMovie method to search movies using MovieDB API
    - Adding query for with parameter 'api_key', 'search', and 'page'

--- app/src/main/java/com/example/finalmobile/MovieListActivity.java
+++ app/src/main/java/com/example/finalmobile/MovieListActivity.java
@@ -3,12 +3,33 @@ package com.example.finalmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.finalmobile.request.Service;
+import com.example.finalmobile.response.MovieSearchResponse;
+import com.example.finalmobile.utils.MovieApi;
+
+import retrofit2.Call;
 
 public class MovieListActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GetRetrofitResponse();
+            }
+        });
+    }
+
+    private void GetRetrofitResponse() {
+        MovieApi movieApi = Service.getMovieApi();
+        Call<MovieSearchResponse> responseCall = movieApi
+                .searchMovie()
     }
 }
--- app/src/main/java/com/example/finalmobile/utils/MovieApi.java
+++ app/src/main/java/com/example/finalmobile/utils/MovieApi.java
@@ -1,4 +1,20 @@
 package com.example.finalmobile.utils;
 
+import com.example.finalmobile.response.MovieSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface MovieApi {
+
+    //Search For Movies
+    //https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key=72f23821b529b1058cffcd50b9cdff27
+    @GET
+    Call<MovieSearchResponse> searchMovie(
+            @Query("api_key") String key,
+            @Query("query") String query,
+            @Query("page") String page
+    );
+
 }
