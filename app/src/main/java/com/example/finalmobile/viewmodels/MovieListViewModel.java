9354421294cd4745de261e626a546c61e9e5b945
AgungArdhiyanda <ardhiyandaak21h@student.unhas.ac.id>
2023-06-07 23:07:47 +0800

    refactor: Update MovieListViewModel class

--- app/src/main/java/com/example/finalmobile/viewmodels/MovieListViewModel.java
+++ app/src/main/java/com/example/finalmobile/viewmodels/MovieListViewModel.java
@@ -1,4 +1,22 @@
 package com.example.finalmobile.viewmodels;
 
+import androidx.lifecycle.LiveData;
+
+import com.example.finalmobile.models.MovieModel;
+import com.example.finalmobile.repositories.MovieRepository;
+
+import java.util.List;
+
 public class MovieListViewModel {
+    private MovieRepository movieRepository;
+
+    public MovieListViewModel(){
+        movieRepository = MovieRepository.getInstance();
+    }
+    public LiveData<List<MovieModel>> getMovies(){
+        return movieRepository.getMovies();
+    }
+    public void searchMovieApi(String query, int pageNumber){
+        movieRepository.searchMovieApi(query, pageNumber);
+    }
 }
