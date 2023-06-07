4adddfad9dab1c39f608a92271d4df5753653bcc
AgungArdhiyanda <ardhiyandaak21h@student.unhas.ac.id>
2023-06-07 19:51:07 +0800

    Feat: Add MovieListFragment class and Fragment method to MainActivity class
    - Add method to call MovieListFragment class in MainActivity class
    - adds the MovieListFragment class, which is responsible for displaying a list of movies in a fragment.

---
+++ app/src/main/java/MovieListFragment.java
@@ -0,0 +1,2 @@
+public class MovieListFragment {
+}
--- app/src/main/java/com/example/finalmobile/MainActivity.java
+++ app/src/main/java/com/example/finalmobile/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.finalmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public ImageView movieBtn, tvBtn, favBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        initiateViews();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        MovieListFragment movieListFragment = new MovieListFragment();
+        Fragment fragment =
+                fragmentManager.findFragmentByTag(MovieListFragment.class.getSimpleName());
+        if(!(fragment instanceof MovieListFragment)){
+            fragmentManager.beginTransaction().add(R.id.frame_layout, movieListFragment
+            ,MovieListFragment.class.getSimpleName()).commit();
+        }
 
 
+
+    }
+
+
+    private void initiateViews(){
+        movieBtn = findViewById(R.id.movieBtn);
+        tvBtn = findViewById(R.id.tvBtn);
+        favBtn = findViewById(R.id.favouriteBtn);
     }
 }
